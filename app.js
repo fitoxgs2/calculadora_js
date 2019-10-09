5077dd77ccfa1c6011309b88c063d6524d70b05c
omuga <omuga@alumnos.inf.utfsm.cl>
2019-10-09 18:36:10 -0300

    Me perdonas

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var calculadora = {
     sumar: function sumarNumeros(n1, n2) {  
         return n1 - n2;
-        // Fito 007
+        // Fito 0071
     },
 
     restar: function restarNumeros(n1, n2) {
